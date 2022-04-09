b9c634001677db3fd64e6cc474938c1973f6c1b6
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-09 11:15:58 -0300

    test: move business logic to a domain entity

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -5,7 +5,9 @@ import {
 } from '@/data/contracts/repos';
 import { AuthenticationError } from '@/domain/errors';
 import { FacebookAuthentication } from '@/domain/features';
+import { FacebookAccount } from '@/domain/models';
 
+// This service used only orchestrations calls abstractions
 export class FacebookAuthenticationService {
   constructor(
     private readonly facebookApi: LoadFacebookUserApi,
@@ -19,13 +21,8 @@ export class FacebookAuthenticationService {
 
     if (fbData !== undefined) {
       const accountData = await this.userAccount.load({ email: fbData.email });
-
-      await this.userAccount.saveWithFacebook({
-        id: accountData?.id,
-        name: accountData?.name ?? fbData.name,
-        email: fbData.email,
-        facebookId: fbData.facebookId,
-      });
+      const fbAccount = new FacebookAccount(fbData, accountData);
+      await this.userAccount.saveWithFacebook(fbAccount);
     }
 
     return new AuthenticationError();
