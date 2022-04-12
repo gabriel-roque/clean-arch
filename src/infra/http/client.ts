08aead2bd3f673b17df3aa1fdc70dad4d50ca196
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-12 15:09:27 -0300

    feat: improve code quality

--- src/infra/http/client.ts
+++ src/infra/http/client.ts
@@ -1,12 +1,10 @@
+export interface HttpGetClient {
+  get: <T = any>(params: HttpGetClient.Params) => Promise<T>;
+}
+
 export namespace HttpGetClient {
   export type Params = {
     url: string;
     params: object;
   };
-
-  export type Result = any;
-}
-
-export interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<HttpGetClient.Result>;
 }
