1d038957b3010e2be9cb90fe18a94858d7363644
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-12 18:27:46 -0300

    test: isolate SUT creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -10,20 +10,26 @@ export class AxiosHttpClient {
 }
 
 describe(AxiosHttpClient.name, () => {
+  let sut: AxiosHttpClient;
+  let fakeAxios: jest.Mocked<typeof axios>;
+  let url: string;
+  let params: object;
+
+  beforeAll(() => {
+    url = 'any_url';
+    params = { any: 'any' };
+    fakeAxios = axios as jest.Mocked<typeof axios>;
+  });
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient();
+  });
+
   describe('Method GET', () => {
     it('should call get with correct params', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>;
-      const sut = new AxiosHttpClient();
+      await sut.get({ url, params });
 
-      await sut.get({
-        url: 'any_url',
-        params: { any: 'any' },
-      });
-
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: { any: 'any' },
-      });
-
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params });
       expect(fakeAxios.get).toHaveBeenCalledTimes(1);
     });
   });
