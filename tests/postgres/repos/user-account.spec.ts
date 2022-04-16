81de8a13e7589c3087c9ee3aff79e768872ce9c0
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-16 15:34:48 -0300

    test: isolate SUT creation

--- tests/postgres/repos/user-account.spec.ts
+++ tests/postgres/repos/user-account.spec.ts
@@ -1,6 +1,13 @@
 import { LoadUserAccountRepository } from '@/data/contracts/repos';
-import { newDb } from 'pg-mem';
-import { Column, Entity, getRepository, PrimaryGeneratedColumn } from 'typeorm';
+import { IBackup, newDb } from 'pg-mem';
+import {
+  Column,
+  Entity,
+  getConnection,
+  getRepository,
+  PrimaryGeneratedColumn,
+  Repository,
+} from 'typeorm';
 
 class PgUserAccountRepository implements LoadUserAccountRepository {
   async load(
@@ -33,24 +40,42 @@ export class PgUser {
 
 describe(PgUserAccountRepository.name, () => {
   describe('Method load()', () => {
-    it('should return an account if email exists', async () => {
+    let sut: PgUserAccountRepository;
+    let pgUserRepo: Repository<PgUser>;
+    let backup: IBackup;
+
+    beforeAll(async () => {
       const db = newDb();
-      db.public.registerFunction({
-        implementation: () => 'test',
-        name: 'current_database',
-      });
       const connection = await db.adapters.createTypeormConnection({
         type: 'postgres',
         entities: [PgUser],
       });
-      // // // create schema
+
       await connection.synchronize();
-      const pgUserRepo = getRepository(PgUser);
-      await pgUserRepo.save({ email: 'existing_email' });
-      const sut = new PgUserAccountRepository();
-      const account = await sut.load({ email: 'existing_email' });
+      backup = db.backup();
+      pgUserRepo = getRepository(PgUser);
+    });
+
+    afterAll(async () => {
+      await getConnection().close();
+    });
+
+    beforeEach(() => {
+      backup.restore();
+      sut = new PgUserAccountRepository();
+    });
+
+    it('should return an account if email exists', async () => {
+      await pgUserRepo.save({ email: 'any_email' });
+      const account = await sut.load({ email: 'any_email' });
 
       expect(account).toEqual({ id: '1' });
+    });
+
+    it('should return undefined if email does not exists', async () => {
+      const account = await sut.load({ email: 'any_email' });
+
+      expect(account).toBeUndefined();
     });
   });
 });
