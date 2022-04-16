5bcae250e1a5bf1f03bc2e5aa5c05d74f0c4b951
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-16 10:36:39 -0300

    feat: AxiosHttpClient implements HttpClient

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@/infra/http';
 import axios from 'axios';
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpClient {
   async get(args: HttpClient.Params): Promise<any> {
     const result = await axios.get(args.url, { params: args.params });
     return result.data;
