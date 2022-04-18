3d39575aff45014de46e87e0cf128f78bbe041f3
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-18 16:20:32 -0300

    test: create validator strings

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -8,6 +8,7 @@ import {
 } from '@/application/helpers';
 import { FacebookAuthentication } from '@/domain/features';
 import { AccessToken } from '@/domain/models';
+import { RequiredStringValidator } from '../validation';
 
 type HttpRequest = {
   token: string;
@@ -45,12 +46,7 @@ export class FacebookLoginController {
   }
 
   private validate(httpRequest: HttpRequest): Error | undefined {
-    if (
-      httpRequest.token === '' ||
-      httpRequest.token === null ||
-      httpRequest.token === undefined
-    ) {
-      return new RequiredFieldError('token');
-    }
+    const validator = new RequiredStringValidator(httpRequest.token, 'token');
+    return validator.validate();
   }
 }
