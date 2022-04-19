e045f3f7c1520fac2ee99a4a55b1b30066b24291
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-19 10:59:36 -0300

    refect: organize imports facebook-login controllers

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -5,9 +5,9 @@ import {
   serverError,
   unauthorized,
 } from '@/application/helpers';
+import { RequiredStringValidator } from '@/application/validation';
 import { FacebookAuthentication } from '@/domain/features';
 import { AccessToken } from '@/domain/models';
-import { RequiredStringValidator } from '../validation';
 
 type HttpRequest = {
   token: string;
