360306d94b1da86222e63fac874730a5a0ca45f0
gabriel-roque <gabrielrms.dll@gmail.com>
2022-04-23 13:05:18 -0300

    chore: config setup integration tests

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,18 @@
-
 module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/index.ts'
+    '!<rootDir>/src/**/index.ts',
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1'
+    '@/(.+)': '<rootDir>/src/$1',
   },
   testMatch: ['**/*.spec.ts'],
-  roots: [
-    '<rootDir>/src',
-    '<rootDir>/tests'
-  ],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   transform: {
-    '\\.ts$': 'ts-jest'
-  }
-}
+    '\\.ts$': 'ts-jest',
+  },
+};
